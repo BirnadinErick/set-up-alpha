5eefd034016413cbd962622dadc791460b7f7224
BirnadinErick <45619033+BirnadinErick@users.noreply.github.com>
2025-11-15 13:48:15 +0100

    feat: add gitlab keygen

---
+++ 04_02_setup_thd_gitlab.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+ssh-keygen -t ed25519 -C "birnadin.anton-robert-clive@stud.th-deg.de" -f /home/be/.ssh/gitlab_key
+#ssh-keygen -t ed25519 -C "birnadin.anton-robert-clive@stud.th-deg.de" -f gitlab_key_sign
+
