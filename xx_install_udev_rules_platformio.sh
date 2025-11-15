d9a1f925b0d69d754460d0f0c12394c722d57e4c
BirnadinErick <45619033+BirnadinErick@users.noreply.github.com>
2025-11-15 13:52:44 +0100

    feat: add udev rules script

---
+++ xx_install_udev_rules_platformio.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+# check URL
+curl -fsSL https://raw.githubusercontent.com/platformio/platformio-core/develop/platformio/assets/system/99-platformio-udev.rules | sudo tee /etc/udev/rules.d/99-platformio-udev.rules
+sudo service udev restart
