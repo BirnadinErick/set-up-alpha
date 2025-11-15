e47f11da05cabaae5131f215f908d75e6ea3131b
BirnadinErick <45619033+BirnadinErick@users.noreply.github.com>
2025-11-15 13:09:24 +0100

    feat: add dconf editor to core apps

--- 01_install_core.sh
+++ 01_install_core.sh
@@ -6,7 +6,7 @@ sudo dnf install nvim -y
 # some other 
 # fzf / bat / rg / btop
 sudo dnf install -y fzf bat ripgrep btop
-flatpak install flathub ca.desrt.dconf-editor -y
+#flatpak install flathub ca.desrt.dconf-editor -y
 
 # default browser
 # flatpak install flathub app.zen_browser.zen
